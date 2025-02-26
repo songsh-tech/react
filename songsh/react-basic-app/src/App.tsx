ab981781bf4e4c9a39a4f0f60f8146226a38692a
songsh-tech <hunsiskj16@naver.com>
2025-02-26 12:39:59 +0900

    2025.02.26. 12:40

--- songsh/react-basic-app/src/App.tsx
+++ songsh/react-basic-app/src/App.tsx
@@ -7,6 +7,8 @@ import Gallery from './component-manage/example/Example1';
 import Profile from './component-manage/example/Example2';
 import ConditionRender from './component-manage/ConditionRender';
 import ListRender from './component-manage/ListRender';
+import EventComponent from './interaction/EventComponent';
+import StateComponent from './interaction/StateComponent';
 
 function App() {
   return (
@@ -18,7 +20,9 @@ function App() {
       {/* <Properties /> */}
       {/* <Profile /> */}
       {/* <ConditionRender /> */}
-      <ListRender />
+      {/* <ListRender /> */}
+      {/* <EventComponent /> */}
+      <StateComponent />
     </div>
   );
 }
