754fd5fa33b1b5c1cb7cfcbe5a360880ac5cea89
songsh-tech <hunsiskj16@naver.com>
2025-02-27 12:48:04 +0900

    2025.02.27. 12:49

--- songsh/react-basic-app/src/App.tsx
+++ songsh/react-basic-app/src/App.tsx
@@ -10,6 +10,7 @@ import ListRender from './component-manage/ListRender';
 import EventComponent from './interaction/EventComponent';
 import StateComponent from './interaction/StateComponent';
 import ForwardingComponent from './interaction/ForwardingComponent';
+import HookComponent1 from './hook/HookComponent1';
 
 function App() {
   return (
@@ -24,7 +25,9 @@ function App() {
       {/* <ListRender /> */}
       {/* <EventComponent /> */}
       {/* <StateComponent /> */}
-      <ForwardingComponent />
+      {/* <ForwardingComponent /> */}
+      <HookComponent1 />
+
     </div>
   );
 }
